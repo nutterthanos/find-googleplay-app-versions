62f96304e925619e83601648918270ad8b00f9a6
GitHub Action <action@github.com>
2024-11-15 01:35:34 +0000

    Updated versions as of 2024-11-15 01:35:34 UTC

--- download.py
+++ download.py
@@ -127,8 +127,8 @@ async def fetch_and_save(session, url, vc, semaphore):
 
 # Main async function
 async def main():
-    version_code_start = 431000000
-    version_code_end = 431500000
+    version_code_start = 431500000
+    version_code_end = 432000000
     max_concurrent_requests = 100
     semaphore = asyncio.Semaphore(max_concurrent_requests)
 
